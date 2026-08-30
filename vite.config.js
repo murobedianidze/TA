eb593b0ade3a66a271fa05daf60ae3284b2296e3
Muro Bedianidze <123646281+murobedianidze@users.noreply.github.com>
2026-08-30 12:48:03 +0400

    changed

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,7 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/TA/', // <-- ჩაამატეთ ეს ხაზი (თქვენი რეპოზიტორიის სახელი)
+  base: '/', // <-- ჩაამატეთ ეს ხაზი (თქვენი რეპოზიტორიის სახელი)
   server: {
     open: true, // <--- ეს ავტომატურად გახსნის შენს ნაგულისხმევ (Default) ბრაუზერს
     port: 5173  // (ან შენი პორტი რაც არის)
