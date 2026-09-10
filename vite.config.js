084bbba7f3bff1df83ff17f8f2902acd8b5731ce
Muro Bedianidze <123646281+murobedianidze@users.noreply.github.com>
2026-09-10 11:22:33 +0400

    changed

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,7 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/TA/', // <-- ჩაამატეთ ეს ხაზი (თქვენი რეპოზიტორიის სახელი)
+  base: process.env.VERCEL ? "/" : "/TA/",
   server: {
     open: true, // <--- ეს ავტომატურად გახსნის შენს ნაგულისხმევ (Default) ბრაუზერს
     port: 5173  // (ან შენი პორტი რაც არის)
